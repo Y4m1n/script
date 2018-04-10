a5083cf27d8536901ec5a3ecea9654ecb8d99d05
Yamin <kungyamin@gmail.com>
2018-04-10 15:38:37 +0800

    修改不规范连结符号

--- rename.py
+++ rename.py
@@ -1,7 +1,8 @@
 # coding:utf-8
 import os
-folder=raw_input('输入作业文件夹名:')
-path='/Users/y4m1n/Downloads/'+folder
+# folder=raw_input('输入作业文件夹名:')
+# path='/Users/y4m1n/Downloads/'+folder
+path='/Users/y4m1n/Downloads/test'
 last_name=['方','孔','杨','胡','牛','王','陈','张','宋','董','付',
            '李','王','方','卫','孙','崔','文','钟','吴','唐','俞',
            '曾','刘','连','于','潘','黄','罗','苏','马','程','郑','蔡']
@@ -11,6 +12,10 @@ for file in os.listdir(path):
     if file.find('.')>0:
         postfix = file.split('.')[-1]
         file_name = file.strip().split('.')[0]
+        # 修改不规范连结符号
+        if file_name.find('_') or file_name.find('+'):
+            file_name=file_name.replace('_','-')
+            file_name=file_name.replace('+','-')
         for name_part in file_name.split('-'):
             # 找学号
             if name_part.isdigit():
@@ -19,13 +24,12 @@ for file in os.listdir(path):
                 if len(id) > 2:
                     id = id[-2:]
             # 找姓名
-            elif name_part[0] in last_name:
+            elif name_part[:3] in last_name:
                 name = name_part
-            else:
-                print 'Wrong file name: '+file
-            # 新名字
-            new_name=id+'-'+name+'.'+postfix
-            file_path = os.path.join(path, file)
-            new_file_path = os.path.join(path, new_name)
-            # 改名
-            os.rename(file_path, new_file_path)
+
+        # 新名字
+        new_name=str(id) + '-' + name + '.' + postfix
+        file_path = os.path.join(path, file)
+        new_file_path = os.path.join(path, new_name)
+        # 改名
+        os.rename(file_path, new_file_path)
