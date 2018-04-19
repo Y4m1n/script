8cbd89e8e2bd7c94028153163679e3a66ff3568d
Yamin <kungyamin@gmail.com>
2018-04-19 17:45:01 +0800

    修复.strip()带来的bug：不能移除所有空格

--- rename.py
+++ rename.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python
 # coding:utf-8
 import os
 folder=raw_input('输入作业文件夹名:')
@@ -10,23 +11,28 @@ for file in os.listdir(path):
     # 找有后缀的
     if file.find('.')>0:
         postfix = file.split('.')[-1]
-        # 去括号，去掉后缀，去掉多余的“号”
-        file_name = file.strip().split('.')[0].replace('号','')
+        # 去空格，去掉后缀，去掉多余的“号”
+        print "file.replace():"+file.replace(" ","")
+        file_name = file.replace(" ","").split('.')[0].replace('号','')
+        print "file_name: "+file_name
         # 修改不规范连结符号
-        if file_name.find('_') or file_name.find('+'):
+        if file_name.find('_') or file_name.find('+') or file_name.find('－'):
             file_name=file_name.replace('_','-')
             file_name=file_name.replace('+','-')
+            file_name=file_name.replace('－','-')
         for name_part in file_name.split('-'):
             # 找学号
+            print "name_part: "+name_part
             if name_part.isdigit():
                 id = name_part
                 # 学号没写两位的则只保留两位
                 if len(id) > 2:
                     id = id[-2:]
+                print "ID: "+id
             # 找姓名
             elif name_part[:3] in last_name:
                 name = name_part
-
+                print "name: "+name
         # 新名字
         new_name=str(id) + '-' + name + '.' + postfix
         file_path = os.path.join(path, file)
