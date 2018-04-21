3e8156a311bc65c311591c9e0ac7679cafa69e69
Youming K <kungyamin@gmail.com>
2018-04-21 11:21:13 +0800

    增加人数参数

--- rename.rb
+++ rename.rb
@@ -3,7 +3,9 @@
 last_name = ['方','孔','杨','胡','牛','王','陈','张','宋','董','付',
            '李','王','方','卫','孙','崔','文','钟','吴','唐','俞',
            '曾','刘','连','于','潘','黄','罗','苏','马','程','郑','蔡']
-id_list = (1..47).to_a
+# 可根据人数调整此参数
+num_of_classmates = 47
+id_list = (1..num_of_classmates).to_a
 Dir.glob("#{ARGV[0]}/**/*") .each do |file_name|
   id = /\d{2,}/.match(file_name).to_s[-2..-1]
   id_list.delete(id.to_i)
