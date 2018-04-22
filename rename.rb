1d7a402173cdf98b033873c03549af0ba2c77f2a
Youming K <kungyamin@gmail.com>
2018-04-23 02:06:09 +0800

    Revert "新增姓氏文本，更新姓的列表获取"
    This reverts commit bbacf9a93369fe7f306e3814607427271da02cea.

--- rename.rb
+++ rename.rb
@@ -1,13 +1,15 @@
 #! /usr/local/bin/ruby
 # coding:utf-8
-family_names=File.read("family_names.txt").split(" ")
+last_name = ['方','孔','杨','胡','牛','王','陈','张','宋','董','付',
+           '李','王','方','卫','孙','崔','文','钟','吴','唐','俞',
+           '曾','刘','连','于','潘','黄','罗','苏','马','程','郑','蔡']
 # 可根据人数调整此参数
 num_of_classmates = 47
 id_list = (1..num_of_classmates).to_a
 Dir.glob("#{ARGV[0]}/**/*") .each do |file_name|
   id = /\d{2,}/.match(file_name).to_s[-2..-1]
   id_list.delete(id.to_i)
-  name = file_name.sub("号","").scan(/[\u4e00-\u9fa5]{2,3}/).find {|text| family_names.include?(text[0])}
+  name = file_name.sub("号","").scan(/[\u4e00-\u9fa5]{2,3}/).find {|text| last_name.include?(text[0])}
   new_name = file_name.sub(File.basename(file_name), id.to_s + "-" + name.to_s + File.extname(file_name))
   File.rename(file_name,new_name)
 end
