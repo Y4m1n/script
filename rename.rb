a77f604d07f7640855d695639302c057c98edddf
Youming K <kungyamin@gmail.com>
2018-04-23 10:15:21 +0800

    处理获取到多个姓氏的情况

--- rename.rb
+++ rename.rb
@@ -8,8 +8,9 @@ id_list = (1..num_of_classmates).to_a
 Dir.glob("#{ARGV[0]}/**/*") .each do |file_name|
   id = /\d{2,}/.match(file_name).to_s[-2..-1]
   id_list.delete(id.to_i)
-  name = file_name.sub("号","").scan(/[\u4e00-\u9fa5]{2,3}/).find {|text| family_names.include?(text[0]) unless teacher_names.include?(text[1..-1])}
-  new_name = file_name.sub(File.basename(file_name), id.to_s + "-" + name.to_s + File.extname(file_name))
+  name = file_name.sub("号","").scan(/[\u4e00-\u9fa5]{2,3}/).select {|text| family_names.include?(text[0]) unless teacher_names.include?(text[1..-1])}
+  puts "WARNING: more than one names got: ",name if name.size>1
+  new_name = file_name.sub(File.basename(file_name), id.to_s + "-" + name.join('-') + File.extname(file_name))
   File.rename(file_name,new_name)
 end
 puts "没交作业的同学学号："
