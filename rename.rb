1048ed8eabd5cfe0412afecf7eab63acc3db9b29
Youming K <kungyamin@gmail.com>
2018-04-23 09:48:27 +0800

    过滤老师名字

--- rename.rb
+++ rename.rb
@@ -1,13 +1,14 @@
 #! /usr/local/bin/ruby
 # coding:utf-8
 family_names=File.read("family_names.txt").split(" ")
+teacher_names=["老师","大嘴"]
 # 可根据人数调整此参数
 num_of_classmates = 47
 id_list = (1..num_of_classmates).to_a
 Dir.glob("#{ARGV[0]}/**/*") .each do |file_name|
   id = /\d{2,}/.match(file_name).to_s[-2..-1]
   id_list.delete(id.to_i)
-  name = file_name.sub("号","").scan(/[\u4e00-\u9fa5]{2,3}/).find {|text| family_names.include?(text[0])}
+  name = file_name.sub("号","").scan(/[\u4e00-\u9fa5]{2,3}/).find {|text| family_names.include?(text[0]) unless teacher_names.include?(text[1..-1])}
   new_name = file_name.sub(File.basename(file_name), id.to_s + "-" + name.to_s + File.extname(file_name))
   File.rename(file_name,new_name)
 end
