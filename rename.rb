1f7d6299108e134e0da76dc4bd87b1daa395cedc
Youming K <kungyamin@gmail.com>
2018-04-25 11:59:15 +0800

    增加字数判断以修正姓名抓取问题

--- rename.rb
+++ rename.rb
@@ -1,7 +1,6 @@
 #! /usr/local/bin/ruby
 # Work with family_names.txt
 family_names = File.read(File.join(File.dirname(__FILE__),'family_names.txt')).split(' ')
-# TO DO: fix issues with "程" using capture group.
 teacher_names = ['老师', '大嘴']
 # 可根据人数调整此参数
 num_of_classmates = 47
@@ -9,7 +8,7 @@ id_list = (1..num_of_classmates).to_a
 Dir.glob("#{ARGV[0]}/**/*") .each do |file_name|
   id = /\d{2,}/.match(file_name).to_s[-2..-1]
   id_list.delete(id.to_i)
-  name = file_name.sub('号', '').scan(/[\u4e00-\u9fa5]{2,3}/).select { |text| family_names.include?(text[0]) unless teacher_names.include?(text[1..-1]) }
+  name = file_name.delete('号').scan(/[\u4e00-\u9fa5]{2,}/).select { |text| (text.length < 4 ) && (family_names.include?(text[0])) unless teacher_names.include?(text[1..-1]) }
   puts 'WARNING: more than one names got: ', name if name.size > 1
   new_name = file_name.sub(File.basename(file_name), id.to_s + '-' + name.join('-') + File.extname(file_name))
   File.rename(file_name, new_name)
